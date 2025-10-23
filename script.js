fb6600392976c59da56cf23eca8ebd29ed0c5946
Anurag Sulkiya <163811905+Anurag-Sulkiya@users.noreply.github.com>
2025-10-23 09:19:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	let area = 0;
+	constructor(width, height){
+        this.width = width;
+		this.height = height;
+	}
 
-class Square extends Animal {}
+	get width(){
+      return this.width = width;
+	}
+
+	get height(){
+       return this.height = height
+	}
+	get width(){
+       return this.width = width;
+	}
+	
+	getArea(){
+		let area = this.width * this.height;
+	}
+}
+
+class Square extends Rectangle {
+	let side;
+	let perimeter;
+	constructor(width, height){
+       super(height, width);
+	}
+	
+    side = this.height = this.width;
+	getPerimeter(){
+		perimeter = 4*this.side;
+	}
+	
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
